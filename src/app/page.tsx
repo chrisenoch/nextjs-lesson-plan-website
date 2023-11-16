2e71d564e1051f8b62b7cc4386a87e1dd324edd1
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-11-16 12:45:58 +0100

    AppBar logic extracted from main Layout and put into ResponsiveAppBar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import Drawer from '@mui/material/Drawer';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import MediaCard from '@/components/MediaCard';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import Drawer from "@mui/material/Drawer";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import MediaCard from "@/components/MediaCard";
 
 export default function HomePage() {
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <div>
         <Alert severity="info" sx={{ mt: 2, mb: 5 }}>
           <AlertTitle>Hello 👋</AlertTitle>
@@ -44,29 +44,6 @@ export default function HomePage() {
           </Grid>
         </Grid>
       </div>
-      <Drawer
-        sx={{
-          width: 320,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: 320,
-            boxSizing: 'border-box',
-            top: ['48px', '56px', '64px'],
-            height: 'auto',
-            bottom: 0,
-          },
-        }}
-        variant="permanent"
-        anchor="right"
-      >
-        <List sx={{ px: 2 }}>
-          <ListItem disablePadding>
-            <Typography variant="overline" sx={{ fontWeight: 500 }}>
-              On this page
-            </Typography>
-          </ListItem>
-        </List>
-      </Drawer>
     </Box>
   );
 }
---
+++ src/components/ResponsiveAppBar.tsx
@@ -0,0 +1,76 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import Link from "next/link";
+
+export default function ResponsiveAppBar({
+  DRAWER_WIDTH,
+  LINKS,
+  PLACEHOLDER_LINKS,
+}) {
+  return (
+    <>
+      <AppBar position="fixed" sx={{ zIndex: 2000 }}>
+        <Toolbar sx={{ backgroundColor: "background.paper" }}>
+          <DashboardIcon
+            sx={{ color: "#444", mr: 2, transform: "translateY(-2px)" }}
+          />
+          <Typography variant="h6" color="text.primary">
+            Next.js App Router
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: DRAWER_WIDTH,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: DRAWER_WIDTH,
+            boxSizing: "border-box",
+            top: ["48px", "56px", "64px"],
+            height: "auto",
+            bottom: 0,
+          },
+        }}
+        variant="permanent"
+        anchor="left">
+        <Divider />
+        <List>
+          {LINKS.map(({ text, href, icon: Icon }) => (
+            <ListItem key={href} disablePadding>
+              <ListItemButton component={Link} href={href}>
+                <ListItemIcon>
+                  <Icon />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider sx={{ mt: "auto" }} />
+        <List>
+          {PLACEHOLDER_LINKS.map(({ text, icon: Icon }) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
+  );
+}
