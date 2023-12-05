231c58b0feb4b46588c6279fc6610cf0bfa3a46b
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-12-05 17:19:15 +0100

    basic Zod client validation added

--- src/components/Jobs/AddJob.tsx
+++ src/components/Jobs/AddJob.tsx
@@ -7,6 +7,7 @@ import useFormClientStatus from "@/customHooks/useFormClientStatus";
 const initialFormState: { message: string | null } = {
   message: null,
 };
+import { z } from "zod";
 
 export function AddJob() {
   console.log("add job rendered");
@@ -16,6 +17,8 @@ export function AddJob() {
   const [showJobDescription, setShowJobDescription] = useState<boolean>(false);
   const jobTitleRef = useRef<null | HTMLInputElement>(null);
   const jobDescriptionRef = useRef<null | HTMLInputElement>(null);
+  const [jobTitle, setJobTitle] = useState<string>("");
+  const [jobDescription, setJobDescription] = useState<string>("");
 
   const inputRefs = useMemo(
     () =>
@@ -26,12 +29,42 @@ export function AddJob() {
     []
   );
   const inputRefsInfo = useFormClientStatus(new Map(inputRefs));
+  const errorMessage = state?.message;
+  const isError = false;
 
   console.log("***inputRefsInfo in AddJob");
   console.log(inputRefsInfo);
 
-  const errorMessage = state?.message;
-  const isError = false;
+  const jobTitleSchema = z.object({
+    jobTitle: z.string().min(2),
+  });
+  const jobDescriptionSchema = z.object({
+    jobDescription: z.string().min(2),
+  });
+
+  if (jobTitleRef.current !== null) {
+    const parse = jobTitleSchema.safeParse({
+      jobTitle: jobTitleRef.current.value,
+    });
+
+    if (!parse.success) {
+      console.log("jobTitle is invalid");
+    } else {
+      console.log("jobTitle is valid");
+    }
+  }
+
+  if (jobDescriptionRef.current !== null) {
+    const parse = jobDescriptionSchema.safeParse({
+      jobDescription: jobDescriptionRef.current.value,
+    });
+
+    if (!parse.success) {
+      console.log("jobDescription is invalid");
+    } else {
+      console.log("jobDescription is valid");
+    }
+  }
 
   return (
     <Box
@@ -73,6 +106,10 @@ export function AddJob() {
           label="Job title"
           variant="outlined"
           inputRef={jobTitleRef}
+          value={jobTitle}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setJobTitle(event.target.value);
+          }}
           error={isError}
           helperText={
             errorMessage && (
@@ -89,6 +126,10 @@ export function AddJob() {
           name="job-description"
           label="Job Description"
           inputRef={jobDescriptionRef}
+          value={jobDescription}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setJobDescription(event.target.value);
+          }}
           multiline
           minRows={4}
         />
