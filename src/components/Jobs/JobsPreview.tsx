2f0423099fa21ccd7383d08732657fc107dd6f8b
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-12-15 11:55:43 +0100

    Jobs are now shown as cards with delete buttons in JobsPreview

--- src/components/Jobs/JobsPreview.tsx
+++ src/components/Jobs/JobsPreview.tsx
@@ -1,8 +1,18 @@
 "use client";
 
 import { useHydrated } from "@/customHooks/useHydrated";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  CardHeader,
+  IconButton,
+} from "@mui/material";
 import { SerializedError } from "@reduxjs/toolkit";
-import { useEffect, useState } from "react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Delete } from "@mui/icons-material";
 
 export function JobsPreview({
   jobs,
@@ -22,16 +32,34 @@ export function JobsPreview({
   //To do: Turn the jobs into links
   const renderedJobs = jobs.jobs.map((job) => {
     return (
-      <li key={job.id}>
-        {job.jobTitle} {job.jobDescription}
-      </li>
+      <>
+        <Card sx={{ mb: 1, maxWidth: 400, minWidth: 300 }} key={job.id}>
+          <CardHeader
+            action={
+              <IconButton
+                onClick={() => console.log("job deleted")}
+                aria-label="delete-job">
+                <Delete />
+              </IconButton>
+            }
+            title={job.jobTitle}
+          />
+          <CardContent>
+            <Typography variant="body2">{job.jobDescription}</Typography>
+          </CardContent>
+        </Card>
+      </>
     );
   });
+
+  console.log("renderedJobs");
+  console.log(renderedJobs);
+
   return jobs.isLoading ? (
     "Loading ..."
   ) : jobs.error ? (
     "Error: There was a problem fetching the jobs. Please reload the page and try again."
   ) : (
-    <ul>{renderedJobs}</ul>
+    <div>{renderedJobs}</div>
   );
 }
