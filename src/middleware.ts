4905c6670cff8fcfa9fa73b7e463b618ebdeefd7
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-12-21 22:48:31 +0100

    unnecessary functions and console.logs from middleware.ts deleted

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,13 +10,10 @@ import {
   isProtectedRouteChildren,
 } from "./models/types/ProtectedRoutes";
 
-// const protectedRoutesAdmin = ["/jobs"];
-// const protectedRoutesUser = ["/lessonplans", "/premium"];
 let count = 0;
 export async function middleware(request: NextRequest) {
   console.log("middleware count " + ++count);
   const originalPath = request.nextUrl.pathname;
-  console.log("originalPath -- " + originalPath);
 
   //Modify path of SecueNextLink
   if (originalPath.includes("/next-link-wrapper-id")) {
@@ -120,68 +117,6 @@ export async function middleware(request: NextRequest) {
 //
 //Helper functions
 
-function handleAuthWithNextWrapperId(
-  request: NextRequest,
-  isAuthenticated: boolean,
-  protectedRoutesByRole: string[],
-  authFailureRelativePath: string
-) {
-  const originalPath = request.nextUrl.pathname;
-
-  console.log("originalPath in handleAuthWithNextWrapperId " + originalPath);
-  if (originalPath.includes("/next-link-wrapper-id")) {
-    console.log("originalPath includes next-link-wrapper-id");
-    const newRelativeUrl = removeSecureNextLinkSuffix(originalPath);
-    console.log("newrelativeUrl : " + newRelativeUrl);
-
-    if (protectedRoutesByRole.includes(newRelativeUrl)) {
-      console.log("if protectedRoutesByRole.includes(newRelativeUrl)");
-      if (isAuthenticated) {
-        console.log("isAuthenticated next-wrapper");
-        console.log("request.url " + request.url);
-        return NextResponse.redirect(new URL(newRelativeUrl, request.url));
-      } else {
-        console.log("is NOT Authenticated next-wrapper");
-        console.log("request.url " + request.url);
-        const urltoRedirectTo = new URL(authFailureRelativePath, request.url);
-        console.log("url to redirect to in next-wrapper: " + urltoRedirectTo);
-        return NextResponse.redirect(
-          new URL(authFailureRelativePath, request.url)
-        );
-      }
-    }
-    //not a protected route, but route includes a next-link-wrapper-id
-    return NextResponse.redirect(new URL(newRelativeUrl, request.url));
-  }
-}
-
-function handleAuth(
-  request: NextRequest,
-  isAuthenticated: boolean,
-  protectedRoutesByRole: string[],
-  authFailureRelativePath: string
-) {
-  const originalPath = request.nextUrl.pathname;
-  if (
-    protectedRoutesByRole.includes(originalPath) &&
-    !originalPath.includes("/next-link-wrapper-id")
-  ) {
-    if (isAuthenticated) {
-      return NextResponse.next();
-    } else {
-      return NextResponse.redirect(
-        new URL(authFailureRelativePath, request.url)
-      );
-    }
-  }
-}
-
-function removeSecureNextLinkSuffix(originalPath: string) {
-  const index = originalPath.indexOf("/next-link-wrapper-id");
-  const newRelativeUrl = originalPath.substring(0, index);
-  return newRelativeUrl;
-}
-
 async function checkPermissions(
   role: UserRole,
   accessToken: string | undefined
@@ -259,29 +194,6 @@ function getAllProtectedRoutes(protectedRoutes: ProtectedRoutes) {
   return allProtectedRoutes;
 }
 
-// function getAllProtectedRoutes(protectedRoutes: ProtectedRoutes) {
-//   const allProtectedRoutes = new Set();
-//   Object.entries(protectedRoutes).forEach(
-//     ([primaryRoute, protectedRoute]: [string, ProtectedRoute]) => {
-//       allProtectedRoutes.add(primaryRoute);
-
-//       if (isProtectedRouteChildren(protectedRoute)) {
-//         Object.values(protectedRoute.children).forEach(
-//           (protectedRouteRolesByRoute: ProtectedRouteRolesByRoute) => {
-//             //loop over the role route-role pairs
-//             Object.keys(protectedRouteRolesByRoute).forEach(
-//               (secondaryRoute: string) => {
-//                 allProtectedRoutes.add(primaryRoute + "/" + secondaryRoute);
-//               }
-//             );
-//           }
-//         );
-//       }
-//     }
-//   );
-//   return allProtectedRoutes;
-// }
-
 function removeStartSlashIfPresent(urlPath: string) {
   if (urlPath.startsWith("/")) {
     return urlPath.substring(1);
@@ -311,13 +223,6 @@ function getPrimaryUrlSegment(urlPath: string) {
   }
 }
 
-//STEP ZERO
-//const allProtectedRoutes: Set<string> = getAllProtectedRoutes(protectedRoutes);
-
-// STEP ONE
-// First check if route protected
-// get all map jkeys, put them in a set and decide if route is protected
-
 // superAdmin field is not if the user is superAdmin. It is the role that you choose
 // which has superAdmin powers (access to everything), if you decide such a role
 // should exist.
@@ -345,7 +250,7 @@ function getUrlPathBasedOnPermissions({
   const allProtectedRoutes: Set<string> =
     getAllProtectedRoutes(protectedRoutes);
 
-  //superAdmin has access to all if superAdmin exists
+  //superAdmin has access to all routes if superAdmin exists
   //To do?: Change superAdmin to an array so that developer can assign multiple superAdmins
   if (superAdmin && userRoles.includes(superAdmin)) {
     return enteredUrlPath;
@@ -386,8 +291,9 @@ function getUrlPathBasedOnPermissions({
       let rolesUserHasForChildrenRoute = [];
       const children = protectedPrimaryRoute.children;
       //Array method toReversed() does not work. Maybe because NextJS middleware uses the Edge environment
-      const childrenReversed = [...children].reverse(); //Search from most specific to least specific and then return the first match.
+      const childrenReversed = [...children].reverse();
 
+      //Search from most specific to least specific and then return the first match.
       childrenBlock: for (const protectedRouteRolesByRoute of childrenReversed) {
         for (const [
           secondaryUrlSegments,
@@ -434,101 +340,8 @@ function getUrlPathBasedOnPermissions({
 
     return notLoggedInRedirectUrlPath;
   }
-
-  // Object.entries(protectedRoutes).forEach(
-  //   ([primaryRoute, protectedRoute]: [string, ProtectedRoute]) => {
-  //     if (primaryRoute === primaryUrlSegment) {
-  //       //if entry does not have children
-  //       if (!isProtectedRouteChildren(protectedRoute)){
-  //         allowedRoles = protectedRoute.roles;
-  //         //return from forEach because the first match
-  //       }
-
-  //     }
-  //   }
-  // );
-
-  // If no 'primaryRoute === primaryUrlSegment' match, return failuReredirectUrl
-  // because there has been an error.
-
-  //loop over protectedRoutes object keys.
-  //If primaryUrlSegment matches any object key:
-  //Check if children exists
-  //----If children DOES NOT exist:
-  //--------Get allowed roles for route -> protectedRoutes.<primaryUlSegment>.roles
-  //--------Check if user role is in returned roles. Use Set Intersection method?
-  //------------if yes, return urlPath
-  //------------If no return failureRedirectUrlPath
-  //----If children EXISTS:
-  //---------reverse the children array
-  //---------loop over children array
-  //------------------loop over each array entry
-  //---------------------------loop over object entries
-  //---------------------------remove any trailing slashes of the key
-  //---------------------------see if urlPath is included in the key
-  //---------------------------first one that matches, return the value (which will be the roles)
-  //---------------------------Check if user role is in returned roles. Use Set Intersection method?
 }
 
-//NEW STRATEGY
-
-//Check if next-wrapper an if so, return the url
-//At the end of this stage we need the relative url to redirect to upon success. Called newRelativeUrl in the next-wrapper function
-//Al ready have the method if includes next-wrapper. Now I need to get it if it does not.
-
-//STEP TWO. get user role (must match to the map keys later).
-//checkPermissions will need to return the roles. Plural
-// can separate with commas and then use string.split method
-//Function: will need to accept an array of user roles.
-
-//STEP THREE - boolean userHasRole/userIsLoggedIn
-
-//let urlToReturn;
-//STEP FOUR
-// if (includes-next-wrapper-id){
-//urltoreturn = getUrlNextWrapper
-// } else {
-//urltoreturn = getUrl
-// }
-
-//Option 1
-//getUrl nextWrapper function
-//Will need to take into account all roles
-// function to get the correct url to return
-
-// object {"Admin": {protectedRoutesAdmin[]
-//                  authFailureRedirectRelativePath:foo,
-//                  incorrectRoleRedirectRelativePath:bar //because if logged in, we redirect to home not login page
-//
-//                   }}
-//          "User": {protectedRoutesUser[]
-//                  authFailureRedirectRelativePath:foo,
-//                  incorrectRoleRedirectRelativePath:bar //because if logged in, we redirect to home not login page
-//
-//                   }}
-
-//Imagine user -> /user-profile     admin admin-profile
-//How decide if admin can access both?
-//Option 1: Admin can access all user routes
-//If so, we just add all other routes to admin route and same as below
-//In this acse, order of map matters
-//Option 2: Must add route to both user and admin
-//Problem with option 2: if add route to user and not to admin. Admin won't be able to access.
-//Option 3 - hybrid: admin accesses all routes below
-//other routes must be added individually
-//Key of "SUPER-ADMIN" - only one with access to all
-//If do not want SUPER-ADMIN to have access to all. Pass undefined or don't include it.
-
-//STEP FIVE
-//Map get routes of all roles.
-
-//STEP SIX
-// add all roles into one array. merge the difefrent arrays.
-
-//STEP SEVEN:
-//If user has several roles, failureCallback will conflict. Solution:1One failureredirect for all.
-// One hasRole but not authorised redirect for all.
-
-//STEP EIGHT
+//To do?
 //Add :ANY PARAM
 //E.g. users/:ANY/profile  :ANY could be any number
