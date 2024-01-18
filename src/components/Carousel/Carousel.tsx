9cd48b27828da3d81f4c291d1e8e37b077eb523b
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-01-18 19:59:48 +0100

    Pictures push each other to the right and left of Carousel with transition.

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Paper, Stack, Typography } from "@mui/material";
+import { Box, Button, Paper, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 import Image from "next/image";
 
@@ -59,91 +59,115 @@ export function Carousel() {
   //     </div>
   //   ));
 
+  const [imageRowRight, setImageRowRight] = useState<number>(800);
+
   return (
-    <Stack direction={"row"}>
-      <Box>Hello</Box>
-      <Box
-        id="image-display-box"
-        width="200px"
-        height="200px"
-        overflow="hidden"
-        position="relative">
-        <Stack
-          direction="row"
-          id="image-row"
-          sx={{
-            width: "200px",
-            height: "200px",
-            backgroundColor: "gray",
-            position: "absolute",
-            right: "800px", //decreasing the value 'right' moves the Images from left to right
-          }}>
-          <Image
-            alt={"beach"}
-            src={
-              "https://raw.githubusercontent.com/chrisenoch/assets/main/beach.jpg"
-            }
-            width={200}
-            height={200}
-            priority
-          />
-          <Image
-            alt={"driverless-cars"}
-            src={
-              "https://raw.githubusercontent.com/chrisenoch/assets/main/driverlesscars.jpg"
-            }
-            width={200}
-            height={200}
-            style={{
-              //marginLeft: "200px",
-              //marginRight: "200px",
-              maxWidth: "100%",
-              height: "auto",
-              objectFit: "cover",
-              //borderRadius: 32,
-            }}
-          />
-          <Image
-            alt={"shopping"}
-            src={
-              "https://raw.githubusercontent.com/chrisenoch/assets/main/shopping.jpg"
-            }
-            width={200}
-            height={200}
-            style={{
-              maxWidth: "100%",
-              height: "auto",
-              objectFit: "cover",
-              //borderRadius: 32,
-            }}
-          />
-          <Image
-            alt={"beach"}
-            src={
-              "https://raw.githubusercontent.com/chrisenoch/assets/main/beach.jpg"
-            }
-            width={200}
-            height={200}
-            priority
-          />
-          <Image
-            alt={"driverless-cars"}
-            src={
-              "https://raw.githubusercontent.com/chrisenoch/assets/main/driverlesscars.jpg"
-            }
-            width={200}
-            height={200}
-            style={{
-              //marginLeft: "200px",
-              marginRight: "200px",
-              maxWidth: "100%",
-              height: "auto",
-              objectFit: "cover",
-              //borderRadius: 32,
-            }}
-          />
-        </Stack>
-      </Box>
-    </Stack>
+    <>
+      <Stack direction={"row"}>
+        <Box>Hello</Box>
+        <Box
+          id="image-display-box"
+          width="200px"
+          height="200px"
+          overflow="hidden"
+          position="relative">
+          <Stack
+            direction="row"
+            id="image-row"
+            sx={{
+              width: "200px",
+              height: "200px",
+              backgroundColor: "gray",
+              transition: "all 1s ease-out",
+              position: "absolute",
+              //right: "800px", //decreasing the value 'right' moves the Images from left to right
+              right: `${imageRowRight}px`,
+            }}>
+            <Image
+              alt={"beach"}
+              src={
+                "https://raw.githubusercontent.com/chrisenoch/assets/main/beach.jpg"
+              }
+              width={200}
+              height={200}
+              priority
+            />
+            <Image
+              alt={"driverless-cars"}
+              src={
+                "https://raw.githubusercontent.com/chrisenoch/assets/main/driverlesscars.jpg"
+              }
+              width={200}
+              height={200}
+              style={{
+                //marginLeft: "200px",
+                //marginRight: "200px",
+                maxWidth: "100%",
+                height: "auto",
+                objectFit: "cover",
+                //borderRadius: 32,
+              }}
+            />
+            <Image
+              alt={"shopping"}
+              src={
+                "https://raw.githubusercontent.com/chrisenoch/assets/main/shopping.jpg"
+              }
+              width={200}
+              height={200}
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+                objectFit: "cover",
+                //borderRadius: 32,
+              }}
+            />
+            <Image
+              alt={"beach"}
+              src={
+                "https://raw.githubusercontent.com/chrisenoch/assets/main/beach.jpg"
+              }
+              width={200}
+              height={200}
+              priority
+            />
+            <Image
+              alt={"driverless-cars"}
+              src={
+                "https://raw.githubusercontent.com/chrisenoch/assets/main/driverlesscars.jpg"
+              }
+              width={200}
+              height={200}
+              style={{
+                //marginLeft: "200px",
+                marginRight: "200px",
+                maxWidth: "100%",
+                height: "auto",
+                objectFit: "cover",
+                //borderRadius: 32,
+              }}
+            />
+          </Stack>
+        </Box>
+      </Stack>
+      <Stack direction={"row"}>
+        <Button
+          onClick={() => setImageRowRight((px) => px + 200)}
+          color="secondary"
+          variant="outlined">
+          Move slide right
+        </Button>
+        <Button
+          onClick={() => setImageRowRight((px) => px - 200)}
+          variant="outlined">
+          Move slide right
+        </Button>
+        <Button
+          onClick={() => setImageRowRight((px) => px - 200)}
+          variant="outlined">
+          Change width of first image
+        </Button>
+      </Stack>
+    </>
   );
 }
