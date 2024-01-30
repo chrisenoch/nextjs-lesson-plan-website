5380c8a6ff8c625aec898e7d7431980397643534
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-01-30 14:40:37 +0100

    Can now pass custom sx props to SearchLessonPlans.

--- src/components/LessonPlans/SearchLessonPlans.tsx
+++ src/components/LessonPlans/SearchLessonPlans.tsx
@@ -1,31 +1,55 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack, SxProps, Theme, Typography } from "@mui/material";
 import { ReactNode } from "react";
 import SVG from "../SVGs/SVG";
 import { underline } from "../SVGs/Paths";
 import { SVGPath } from "../SVGs/StyledSVGPath";
+import { setSXValues } from "@/component-functions/set-sx-values";
 
 export default function SearchLessonPlans({
   searchTitle,
+  sxOuterContainer,
+  sxInnerContainer,
+  sxTitle,
   children,
 }: {
   searchTitle: string;
+  sxOuterContainer?: SxProps<Theme>;
+  sxInnerContainer?: SxProps<Theme>;
+  sxTitle?: SxProps<Theme>;
   children: ReactNode;
 }) {
+  const sxOuterContainerDefault: SxProps<Theme> = {
+    maxWidth: "900px",
+    mt: 10,
+    mb: 4,
+    mx: "auto",
+  };
+  const sxInnerContainerDefault: SxProps<Theme> = { alignSelf: "center" };
+  const sxTitleDefault: SxProps<Theme> = { display: "inline-block" };
+
+  const { sxOuterContainerFinal, sxInnerContainerFinal, sxTitleFinal } =
+    setSXValues([
+      {
+        userValues: sxOuterContainer,
+        defaultValues: sxOuterContainerDefault,
+        sxName: "OuterContainer",
+      },
+      {
+        userValues: sxInnerContainer,
+        defaultValues: sxInnerContainerDefault,
+        sxName: "InnerContainer",
+      },
+      {
+        userValues: sxTitle,
+        defaultValues: sxTitleDefault,
+        sxName: "Title",
+      },
+    ]);
+
   return (
-    <Stack
-      spacing={4}
-      sx={{
-        maxWidth: "900px",
-        mt: 10,
-        mb: 4,
-        mx: "auto",
-      }}>
-      <Stack alignSelf={"center"}>
-        <Typography
-          id="search-lesson-plans"
-          variant="h4"
-          component="h3"
-          display={"inline-block"}>
+    <Stack spacing={4} sx={sxOuterContainerFinal}>
+      <Stack sx={sxInnerContainerFinal}>
+        <Typography variant="h4" component="h3" sx={sxTitleFinal}>
           {searchTitle}
         </Typography>
         <SVG
