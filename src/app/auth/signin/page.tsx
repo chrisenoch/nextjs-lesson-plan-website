d378bdd64e738a263ac36764edaed16f5662a28a
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-01 12:50:52 +0100

    SignIn box is smaller and title added with curved underline.

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,14 +1,25 @@
 import { SignIn } from "@/components/Auth/SignIn";
-import { Stack } from "@mui/material";
+import CurvedUnderlineTitle from "@/components/CurvedUnderline";
+import { Stack, Typography } from "@mui/material";
+import { orange } from "@mui/material/colors";
 
 export default function SignInPage() {
   return (
     <Stack
       spacing={2}
-      maxWidth={"1200px"}
+      maxWidth={"500px"}
       mx={"auto"}
       marginTop={2}
-      marginBottom={2}>
+      marginBottom={2}
+      justifyContent={"center"}>
+      <CurvedUnderlineTitle
+        component={"h1"}
+        variant="h3"
+        title={"Sign In"}
+        color={orange[300]}
+        sxUnderline={{ left: 2, borderRadius: "30%" }}
+        sxTypography={{ marginBottom: "12px !important", alignSelf: "center" }}
+      />
       <SignIn />
     </Stack>
   );
