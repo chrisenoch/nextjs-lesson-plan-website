c4a0429b4660349f5b53d6464ceeb02915cf8df8
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-05 14:41:29 +0100

    Error.tsx at route level styled.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,4 +1,37 @@
 "use client";
-export default function Error() {
-  return <h1>Sorry, there has been an error.</h1>;
+
+import NotificationBox from "@/components/NotificationBox";
+import SecureNextLink from "@/components/Utils/SecureNextLink";
+import { Box, Button, Stack } from "@mui/material";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <Box>
+      <NotificationBox
+        title="Sorry, there has been an error."
+        titleComponent={"h1"}
+        titleVariant={"h3"}
+        message="Please try reloading the page or contact our support team."
+        sxMessage={{
+          fontSize: "1.125rem",
+          marginTop: 3,
+        }}
+        sxInnerContainer={{ borderColor: "transparent" }}>
+        <Stack direction="row" gap={2} marginTop={2}>
+          <Button variant="outlined" onClick={() => reset()}>
+            Try again
+          </Button>
+          <Button variant="outlined" component={SecureNextLink} href={"/"}>
+            Back to home
+          </Button>
+        </Stack>
+      </NotificationBox>
+    </Box>
+  );
 }
