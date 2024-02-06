40de9cd579bd54c081cd99866e366e5a3c047bc9
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-06 14:14:25 +0100

    Unused imports removed from SignIn component.

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,7 +1,4 @@
 import { SignIn } from "@/components/Auth/SignIn";
-import CurvedUnderlineTitle from "@/components/Presentation/CurvedUnderline";
-import { Stack, Typography } from "@mui/material";
-import { orange } from "@mui/material/colors";
 
 export default function SignInPage() {
   return <SignIn />;
