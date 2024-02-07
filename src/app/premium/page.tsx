def3e2ca4fbd4330eedd65175e930e0c8e0e1523
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-07 11:29:44 +0100

    Custom breakpoints set in main MUI theme.ts

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { useState } from "react";
 
 export default function PremiumPage() {
@@ -15,6 +15,26 @@ export default function PremiumPage() {
       marginBottom={2}>
       <p>This is the premium area!</p>
       <button onClick={() => setNum((c) => c + 1)}>Set Num</button>
+      {/* <Box // test custom breakpoints
+        sx={{
+          width: "100%",
+          height: 8,
+          display: {
+            "3sm": "block",
+            "2sm": "block", //430,
+            sm: "none", //600
+            md: "block",
+            lg: "none",
+          }, //also try with
+          backgroundColor: {
+            xs: "primary.main",
+            "3sm": "success.main", //350,
+            "2sm": "warning.main", //430,
+            sm: "secondary.main", //600
+            md: "error.main",
+            lg: "warning.main",
+          },
+        }}></Box> */}
     </Stack>
   );
 }
