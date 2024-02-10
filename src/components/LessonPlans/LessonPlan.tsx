7b6a2b5b1dc8e592043fdab60ac4c04d2e2aebcf
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-10 19:08:46 +0100

    Now use maps to generate contents of LessonPlan page

--- src/components/LessonPlans/LessonPlan.tsx
+++ src/components/LessonPlans/LessonPlan.tsx
@@ -14,7 +14,7 @@ import {
 } from "@mui/material";
 import FloatingId from "@/components/Layout/FloatingId";
 import useRedirectWhenLoggedOut from "@/customHooks/useRedirectWhenLoggedOut";
-import { useMemo } from "react";
+import { Fragment, useMemo } from "react";
 import InsecureNextLink from "next/link";
 import CurvedUnderlineTitle from "../Presentation/CurvedUnderline";
 import { orange } from "@mui/material/colors";
@@ -63,327 +63,220 @@ export default function LessonPlan({
 
   let activeIntersection = useScrollSpy(memoizedTargetElementIds);
 
-  return (
-    <>
-      <Stack
-        direction="row"
-        maxWidth={"1200px"}
-        sx={{
-          mx: { xs: "-8px", sm: "auto" },
-        }}
-        marginTop={2}
-        marginBottom={2}>
-        <Grid
-          container
-          spacing={4}
+  const listItems: {
+    href: string;
+    listItemTextPrimary: string;
+    targetElementId: string;
+  }[] = [
+    {
+      href: "#summary-floating",
+      listItemTextPrimary: "Summary",
+      targetElementId: "summary",
+    },
+    {
+      href: "#warmer-floating",
+      listItemTextPrimary: "Warmer",
+      targetElementId: "warmer",
+    },
+    {
+      href: "#teach-vocabulary-floating",
+      listItemTextPrimary: "Teach Vocabulary",
+      targetElementId: "teach-vocabulary",
+    },
+    {
+      href: "#vocabulary-exercise-floating",
+      listItemTextPrimary: "Vocabulary Exercise",
+      targetElementId: "vocabulary-exercise",
+    },
+    {
+      href: "#teach-speaking-phrases-floating",
+      listItemTextPrimary: "Teach Speaking Phrases",
+      targetElementId: "teach-speaking-phrases",
+    },
+    {
+      href: "#role-play-floating",
+      listItemTextPrimary: "Role Play",
+      targetElementId: "role-play",
+    },
+    {
+      href: "#feedback-floating",
+      listItemTextPrimary: "Feedback",
+      targetElementId: "feedback",
+    },
+    {
+      href: "#plenary",
+      listItemTextPrimary: "Plenary",
+      targetElementId: "plenary",
+    },
+  ];
+
+  const sections: {
+    id: string;
+    sectionTitle: string;
+    floatingId: string;
+    content: string;
+  }[] = [
+    {
+      floatingId: "warmer-floating",
+      sectionTitle: "Warmer",
+      id: "warmer",
+      content: warmer,
+    },
+    {
+      floatingId: "teach-vocabulary-floating",
+      sectionTitle: "Teach Vocabulary",
+      id: "teach-vocabulary",
+      content: teachVocabulary,
+    },
+    {
+      floatingId: "vocabulary-exercise-floating",
+      sectionTitle: "Vocabulary Exercise",
+      id: "vocabulary-exercise",
+      content: vocabularyExercises,
+    },
+    {
+      floatingId: "teach-speaking-phrases-floating",
+      sectionTitle: "Teach Speaking Phrases",
+      id: "teach-speaking-phrases",
+      content: teachSpeakingPhrases,
+    },
+    {
+      floatingId: "role-play-floating",
+      sectionTitle: "Role Play",
+      id: "role-play",
+      content: rolePlay,
+    },
+    {
+      floatingId: "feedback-floating",
+      sectionTitle: "Feedback",
+      id: "feedback",
+      content: feedback,
+    },
+    {
+      floatingId: "plenary-floating",
+      sectionTitle: "Plenary",
+      id: "plenary",
+      content: plenary,
+    },
+  ];
+
+  const renderedSections = sections.map((s) => (
+    <Fragment key={s.id}>
+      <Typography
+        id={s.id}
+        gutterBottom
+        variant="h4"
+        component="h2"
+        position="relative">
+        {s.sectionTitle}
+        <FloatingId id={s.floatingId} />
+      </Typography>
+      <Typography
+        variant="body1"
+        marginBottom={5}
+        sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
+        {s.content}
+      </Typography>
+    </Fragment>
+  ));
+
+  const renderedListitems = listItems.map((listItem) => (
+    <ListItem disablePadding key={listItem.targetElementId}>
+      <ListItemButton component={InsecureNextLink} href={listItem.href}>
+        <ListItemText
+          primary={listItem.listItemTextPrimary}
           sx={{
-            justifyContent: { xs: "center", md: "normal" },
+            "& .MuiListItemText-primary": {
+              fontWeight:
+                activeIntersection === listItem.targetElementId
+                  ? "bold"
+                  : "normal",
+              color: "primary.main",
+            },
+          }}
+        />
+      </ListItemButton>
+    </ListItem>
+  ));
+
+  return (
+    <Stack
+      direction="row"
+      maxWidth={"1200px"}
+      sx={{
+        mx: { xs: "-8px", sm: "auto" },
+      }}
+      marginTop={2}
+      marginBottom={2}>
+      <Grid
+        container
+        spacing={4}
+        sx={{
+          justifyContent: { xs: "center", md: "normal" },
+        }}>
+        <Grid
+          item
+          xs={3.5}
+          sx={{
+            display: { xs: "none", md: "block" },
           }}>
-          <Grid
-            item
-            xs={3.5}
+          <List
             sx={{
-              display: { xs: "none", md: "block" },
-            }}>
-            <List
-              sx={{
-                //width: "100%",
-                width: "fit-content",
-                maxWidth: 360,
-                bgcolor: "background.paper",
-                position: "fixed",
-              }}
-              aria-label="summary">
-              <ListItem disablePadding>
-                <ListItemButton
-                  component={InsecureNextLink}
-                  href="#summary-floating">
-                  {/* <ListItemIcon>
-                  <StarIcon />
-                </ListItemIcon> */}
-                  <ListItemText
-                    primary="Summary"
-                    sx={{
-                      "& .MuiListItemText-primary": {
-                        fontWeight:
-                          activeIntersection === "summary" ? "bold" : "normal",
-                        color: "primary.main",
-                      },
-                    }}
-                  />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton
-                  component={InsecureNextLink}
-                  href="#warmer-floating">
-                  <ListItemText
-                    primary="Warmer"
-                    sx={{
-                      "& .MuiListItemText-primary": {
-                        fontWeight:
-                          activeIntersection === "warmer" ? "bold" : "normal",
-                        color: "primary.main",
-                      },
-                    }}
-                  />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton
-                  component={InsecureNextLink}
-                  href="#teach-vocabulary-floating">
-                  <ListItemText
-                    primary="Teach Vocabulary"
-                    sx={{
-                      "& .MuiListItemText-primary": {
-                        fontWeight:
-                          activeIntersection === "teach-vocabulary"
-                            ? "bold"
-                            : "normal",
-                        color: "primary.main",
-                      },
-                    }}
-                  />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton
-                  component={InsecureNextLink}
-                  href="#vocabulary-exercise-floating">
-                  <ListItemText
-                    primary="Vocabulary Exercise"
-                    sx={{
-                      "& .MuiListItemText-primary": {
-                        fontWeight:
-                          activeIntersection === "vocabulary-exercise"
-                            ? "bold"
-                            : "normal",
-
-                        color: "primary.main",
-                      },
-                    }}
-                  />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton
-                  component={InsecureNextLink}
-                  href="#teach-speaking-phrases-floating">
-                  <ListItemText
-                    primary="Teach Speaking Phrases"
-                    sx={{
-                      "& .MuiListItemText-primary": {
-                        fontWeight:
-                          activeIntersection === "teach-speaking-phrases"
-                            ? "bold"
-                            : "normal",
-                        color: "primary.main",
-                      },
-                    }}
-                  />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton
-                  component={InsecureNextLink}
-                  href="#role-play-floating">
-                  <ListItemText
-                    primary="Role Play"
-                    sx={{
-                      "& .MuiListItemText-primary": {
-                        fontWeight:
-                          activeIntersection === "role-play"
-                            ? "bold"
-                            : "normal",
-                        color: "primary.main",
-                      },
-                    }}
-                  />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton
-                  component={InsecureNextLink}
-                  href="#feedback-floating">
-                  <ListItemText
-                    primary="Feedback"
-                    sx={{
-                      "& .MuiListItemText-primary": {
-                        fontWeight:
-                          activeIntersection === "feedback" ? "bold" : "normal",
-                        color: "primary.main",
-                      },
-                    }}
-                  />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component={InsecureNextLink} href="#plenary">
-                  <ListItemText
-                    primary="Plenary"
-                    sx={{
-                      "& .MuiListItemText-primary": {
-                        fontWeight:
-                          activeIntersection === "plenary" ? "bold" : "normal",
-                        color: "primary.main",
-                      },
-                    }}
-                  />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </Grid>
-          <Grid item xs={10} md={8} bgcolor={"background.paper"}>
-            <CurvedUnderlineTitle
-              component={"h1"}
-              variant="h2"
-              title={title}
-              color={orange[300]}
-              sxUnderline={{ left: 2 }}
-              sxTypography={{
-                marginBottom: 6,
-                display: { xs: "none", sm: "inline-block" },
-              }}
-            />
-            <Typography
-              id="summary"
-              gutterBottom
-              variant="h4"
-              component="h2"
-              position="relative"
-              sx={{ display: { xs: "none", sm: "block" } }}>
-              Summary
-              <FloatingId id="summary-floating" />
-            </Typography>
-            <CurvedUnderlineTitle
-              typographyId="summary"
-              component={"h1"}
-              variant="h4"
-              title={title}
-              color={orange[300]}
-              sxUnderline={{ left: 2, borderRadius: "20%" }}
-              sxTypography={{
-                marginBottom: "0.75em",
-                display: { xs: "inline-block", sm: "none" },
-              }}>
-              <FloatingId id="summary-floating" />
-            </CurvedUnderlineTitle>
-
-            <Typography
-              variant="body1"
-              marginBottom={5}
-              sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
-              {summary}
-            </Typography>
-            <Typography
-              id="warmer"
-              gutterBottom
-              variant="h4"
-              component="h2"
-              position="relative">
-              Warmer
-              <FloatingId id="warmer-floating" />
-            </Typography>
-            <Typography
-              variant="body1"
-              marginBottom={5}
-              sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
-              {warmer}
-            </Typography>
-            <Typography
-              id="teach-vocabulary"
-              gutterBottom
-              variant="h4"
-              component="h2"
-              position="relative">
-              Teach Vocabulary
-              <FloatingId id="teach-vocabulary-floating" />
-            </Typography>
-            <Typography
-              variant="body1"
-              marginBottom={5}
-              sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
-              {teachVocabulary}
-            </Typography>
-            <Typography
-              id="vocabulary-exercise"
-              gutterBottom
-              variant="h4"
-              component="h2"
-              position="relative">
-              Vocabulary exercise
-              <FloatingId id="vocabulary-exercise-floating" />
-            </Typography>
-            <Typography
-              variant="body1"
-              marginBottom={5}
-              sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
-              {vocabularyExercises}
-            </Typography>
-            <Typography
-              id="teach-speaking-phrases"
-              gutterBottom
-              variant="h4"
-              component="h2"
-              position="relative">
-              Teach speaking phrases
-              <FloatingId id="teach-speaking-phrases-floating" />
-            </Typography>
-            <Typography
-              variant="body1"
-              marginBottom={5}
-              sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
-              {teachSpeakingPhrases}
-            </Typography>
-            <Typography
-              id="role-play"
-              gutterBottom
-              variant="h4"
-              component="h2"
-              position="relative">
-              Role play
-              <FloatingId id="role-play-floating" />
-            </Typography>
-            <Typography
-              variant="body1"
-              marginBottom={5}
-              sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
-              {rolePlay}
-            </Typography>
-            <Typography
-              id="feedback"
-              gutterBottom
-              variant="h4"
-              component="h2"
-              position="relative">
-              Feedback
-              <FloatingId id="feedback-floating" />
-            </Typography>
-            <Typography
-              variant="body1"
-              marginBottom={5}
-              sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
-              {feedback}
-            </Typography>
-            <Typography
-              id="plenary"
-              gutterBottom
-              variant="h4"
-              component="h2"
-              position="relative">
-              Plenary
-              <FloatingId id="plenary-floating" />
-            </Typography>
-            <Typography
-              variant="body1"
-              marginBottom={5}
-              sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
-              {plenary}
-            </Typography>
-          </Grid>
+              //width: "100%",
+              width: "fit-content",
+              maxWidth: 360,
+              bgcolor: "background.paper",
+              position: "fixed",
+            }}
+            aria-label="summary">
+            {renderedListitems}
+          </List>
         </Grid>
-      </Stack>
-    </>
+        <Grid item xs={10} md={8} bgcolor={"background.paper"}>
+          <CurvedUnderlineTitle
+            component={"h1"}
+            variant="h2"
+            title={title}
+            color={orange[300]}
+            sxUnderline={{ left: 2 }}
+            sxTypography={{
+              marginBottom: 6,
+              display: { xs: "none", sm: "inline-block" },
+            }}
+          />
+          <Typography
+            id="summary"
+            gutterBottom
+            variant="h4"
+            component="h2"
+            position="relative"
+            sx={{ display: { xs: "none", sm: "block" } }}>
+            Summary
+            <FloatingId id="summary-floating" />
+          </Typography>
+          <CurvedUnderlineTitle
+            typographyId="summary"
+            component={"h1"}
+            variant="h4"
+            title={title}
+            color={orange[300]}
+            sxUnderline={{ left: 2, borderRadius: "20%" }}
+            sxTypography={{
+              marginBottom: "0.75em",
+              display: { xs: "inline-block", sm: "none" },
+            }}>
+            <FloatingId id="summary-floating" />
+          </CurvedUnderlineTitle>
+          <Typography
+            variant="body1"
+            marginBottom={5}
+            sx={{ fontSize: { xs: "0.9375rem", sm: "1rem" } }}>
+            {summary}
+          </Typography>
+
+          {renderedSections}
+        </Grid>
+      </Grid>
+    </Stack>
   );
 }
