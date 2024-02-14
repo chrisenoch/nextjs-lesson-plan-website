6e0b164e39a3d898e3a5cc9a61e994743f7d8d2a
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-14 17:13:56 +0100

    SecureNextLink documented.

--- src/components/Utils/SecureNextLink.tsx
+++ src/components/Utils/SecureNextLink.tsx
@@ -3,20 +3,25 @@ import Link from "next/link";
 import { ReactElement, forwardRef } from "react";
 import { useHydrated } from "@/customHooks/useHydrated";
 
-// Summary: SecureNextLink always invokes middleware (according to tests so far). NextLink sometimes skips middleware.
-
-// When NextLink does not trigger middleware, this is a problem because middleware is used for authentication.
-// Using NextLink leads to a user logging out and still being able to view authenticated content for a period of time.
-// Also, a user may login and not have access to the authenticated content immediately due to NextLink's
-// soft navigation and middleware not being activated.
-
-// SecureNextLink adds a random id to every request, which pushes NextJS to invoke the middleware so we can check for authentication.
-// Then, in the middleware, the random id suffix is deleted and the user is redirected back to the original id.
-// The user does not see any difference.
-
-// The problem with this approach is that a developer may use NextLink by mistake. However, this is the only approach
-// that ensures middleware is activated. NextLink also sometimes skips rendering server components due to its soft navigation.
-// This means that we cannot reliably protect routes in server page components either.
+/**
+ * @summary SecureNextLink always invokes middleware (according to tests so far). NextLink DOES NOT always invoke middleware due to its soft navigation.
+ * Numerous issues have been posted regarding this on StackOverflow, Reddit and GitHub. We use this solution rather than changing the version of Next.js
+ * because sometimes Next.js solves bugs in one version and then they reappear in later versions. To ensure a stable application, we solve the issue ourselves.
+ * @description When NextLink does not trigger middleware, this is a problem for authentication.
+ * Using NextLink leads to issues such as a user logging out and still being able to view authenticated content for a period of time,
+ * a user logging in and not having access to the authenticated content immediately (due to NextLink's soft navigation and middleware not being activated),
+ * and even the wrong url showing in the navigation bar.
+ *
+ * The problem with our SecureNextLink approach is that a developer may use NextLink by mistake. However, this is the only approach that ensures middleware
+ * is activated every time. NextLink also sometimes skips rendering server components due to its soft navigation. This means that we cannot reliably protect
+ * routes in server page components via a HOC either.
+ *
+ * SecureNextLink works by adding a random id to a query parameter in every request, which pushes NextJS to skip soft navigation and invoke the middleware
+ * so we can check for authentication. Then, in the middleware, the query parameter with the random id is deleted and the user is redirected back
+ * to the original url (see handleSecureNextLink# in middleware.ts). The user does not see any difference.
+ *
+ * Limitations: Does not suppport segments. E.g. foo.com/home#title. As segments are not sent to the server.
+ */
 const SecureNextLink = forwardRef(function Wrapper(props: any, ref: any) {
   const {
     href,
