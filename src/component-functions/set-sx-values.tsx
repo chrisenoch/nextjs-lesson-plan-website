e711a6905bdeaefcf88ec2a874a85a883d74824f
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-14 13:44:05 +0100

    set-sx-values# documented

--- src/component-functions/set-sx-values.tsx
+++ src/component-functions/set-sx-values.tsx
@@ -1,5 +1,36 @@
 import { SxProps, Theme } from "@mui/material";
 
+/**
+ * @summary MUI styled components cannot be used in Next.js server components. This function allows us to pass custom props to components in server components.
+ * @description Allow user to pass sx props to custom, with the component defining default sx props if the user does not supply them.
+ * @param sxToSet 
+ * @example - 'Usage in a component, assuming the component accepts sxOuterContainer and sxInnerContainer as props'.
+ *  const sxOuterContainerDefault: SxProps<Theme> = {
+      maxWidth: "900px",
+      mt: 3,
+    };
+     const sxInnerContainerDefault: SxProps<Theme> = {
+    //some values
+  };
+     const {
+    sxOuterContainerFinal, sxInnerContainerFinal
+  } = setSXValues([
+    {
+      userValues: sxOuterContainer,
+      defaultValues: sxOuterContainerDefault,
+      sxName: "OuterContainer",
+    },
+      {
+      userValues: sxInnerContainer,
+      defaultValues: sxInnerContainerDefault,
+      sxName: "InnerContainer",
+    },
+    ]);
+
+    'And then sxOuterContainerFinal and sxInnerContainerFinal can be assigned to the internal component elements as sx props.'
+ * @returns An object with the final SxProps objects. The object values follow a strict naming convention. E.g. If you supply the function with sxOuterContainer,
+    it will return sxOuterContainerFinal.
+ */
 export function setSXValues(
   sxToSet: {
     userValues: SxProps<Theme> | undefined;
