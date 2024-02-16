2862b7c83f1b056247974127100ce39e1d618caf
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-16 13:42:11 +0100

    useHideMessageOnNavAway# documented

--- src/customHooks/useHideMessageOnNavAway.tsx
+++ src/customHooks/useHideMessageOnNavAway.tsx
@@ -1,18 +1,34 @@
 import { useState } from "react";
 
+/**
+ * @example
+ *  const shouldHideMessage = useHideMessageOnNavAway(addJobHttpResponseStatus);
+ * 
+ * {!shouldHideMessage && addJobHttpResponseStatus.message && (
+            <'component that shows the message'>
+    )}
+ * @param httpResponseStatus 
+ * @returns 
+ */
 export default function useHideMessageOnNavAway(
-  formInfo: null | {
+  httpResponseStatus: null | {
     message: string;
     isLoading: boolean;
   }
 ) {
-  const [previousFormInfo, setPreviousFormInfo] = useState<null | {
+  const [previousHttpResponseStatus] = useState<null | {
     message: string;
     isLoading: boolean;
-  }>(formInfo);
+  }>(httpResponseStatus);
 
-  if (formInfo !== previousFormInfo && !formInfo?.isLoading) {
-    if (formInfo?.message) {
+  /*If the http response status has been updated (e.g. due to a response from the server after a form submission) then we do not hide the message.
+  If the user navigates away and then later navigates back to the page, initially httpResponseStatus will equal previousHttpResponseStatus 
+  and so true will be returned. */
+  if (
+    httpResponseStatus !== previousHttpResponseStatus &&
+    !httpResponseStatus?.isLoading
+  ) {
+    if (httpResponseStatus?.message) {
       return false;
     } else {
       return true;
