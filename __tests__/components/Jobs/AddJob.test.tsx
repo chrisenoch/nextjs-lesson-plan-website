e06c916fab8b849063afedd224197ef6998f785c
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-20 16:18:00 +0100

    AddJob.test refactored

--- __tests__/components/Jobs/AddJob.test.tsx
+++ __tests__/components/Jobs/AddJob.test.tsx
@@ -5,71 +5,70 @@ import { AddJob } from "@/components/Jobs/AddJob";
 import { render } from "@/test-utils/render";
 import userEvent from "@testing-library/user-event";
 import { simpleDelay } from "@/utils/delay";
+import { act } from "@testing-library/react";
+
+let jobTitle: HTMLInputElement | null = null;
+let jobLocation: HTMLInputElement | null = null;
+let jobCompany: HTMLInputElement | null = null;
+let jobSalary: HTMLInputElement | null = null;
+let jobDescription: HTMLInputElement | null = null;
+let submitButton: HTMLButtonElement | null = null;
 
 beforeEach(() => {
   render(<AddJob />);
+  jobTitle = screen.getByTestId("jobTitle") as HTMLInputElement;
+  jobLocation = screen.getByTestId("jobLocation") as HTMLInputElement;
+  jobCompany = screen.getByTestId("jobCompany") as HTMLInputElement;
+  jobSalary = screen.getByTestId("jobSalary") as HTMLInputElement;
+  jobDescription = screen.getByTestId("jobDescription") as HTMLInputElement;
+  submitButton = screen.getByTestId("addJobSubmitButton") as HTMLButtonElement;
 });
 
 describe("helper-text shows on click and disappears on valid", () => {
   test("jobTitle - (valid with 2 characters)", async () => {
-    const input = screen.getByTestId("jobTitle") as HTMLInputElement;
-    userEvent.click(input);
-    await simpleDelay(300);
+    await userEvent.click(jobTitle!);
     expect(document.getElementById("jobTitle-helper-text")).toBeInTheDocument();
-    fireEvent.change(input, { target: { value: "123" } });
-    await simpleDelay(300);
+    fireEvent.change(jobTitle!, { target: { value: "123" } });
     expect(
       document.getElementById("jobTitle-helper-text")
     ).not.toBeInTheDocument();
   });
   test("jobLocation - (valid with 2 characters)", async () => {
-    const input = screen.getByTestId("jobLocation") as HTMLInputElement;
-    userEvent.click(input);
-    await simpleDelay(300);
+    await userEvent.click(jobLocation!);
     expect(
       document.getElementById("jobLocation-helper-text")
     ).toBeInTheDocument();
-    fireEvent.change(input, { target: { value: "123" } });
-    await simpleDelay(300);
+    fireEvent.change(jobLocation!, { target: { value: "123" } });
     expect(
       document.getElementById("jobLocation-helper-text")
     ).not.toBeInTheDocument();
   });
   test("jobCompany - (valid with 2 characters)", async () => {
-    const input = screen.getByTestId("jobCompany") as HTMLInputElement;
-    userEvent.click(input);
-    await simpleDelay(300);
+    await userEvent.click(jobCompany!);
     expect(
       document.getElementById("jobCompany-helper-text")
     ).toBeInTheDocument();
-    fireEvent.change(input, { target: { value: "123" } });
-    await simpleDelay(300);
+    fireEvent.change(jobCompany!, { target: { value: "123" } });
     expect(
       document.getElementById("jobCompany-helper-text")
     ).not.toBeInTheDocument();
   });
   test("jobSalary - (valid with 4 characters)", async () => {
-    const input = screen.getByTestId("jobSalary") as HTMLInputElement;
-    userEvent.click(input);
-    await simpleDelay(300);
+    await userEvent.click(jobSalary!);
     expect(
       document.getElementById("jobSalary-helper-text")
     ).toBeInTheDocument();
-    fireEvent.change(input, { target: { value: "1234" } });
-    await simpleDelay(300);
+    fireEvent.change(jobSalary!, { target: { value: "1234" } });
     expect(
       document.getElementById("jobSalary-helper-text")
     ).not.toBeInTheDocument();
   });
   test("jobDescription - (valid with 2 characters)", async () => {
-    const input = screen.getByTestId("jobDescription") as HTMLInputElement;
-    userEvent.click(input);
-    await simpleDelay(300);
+    await userEvent.click(jobDescription!);
     expect(
       document.getElementById("jobDescription-helper-text")
     ).toBeInTheDocument();
-    fireEvent.change(input, { target: { value: "123" } });
-    await simpleDelay(300);
+    fireEvent.change(jobDescription!, { target: { value: "123" } });
     expect(
       document.getElementById("jobDescription-helper-text")
     ).not.toBeInTheDocument();
@@ -78,102 +77,83 @@ describe("helper-text shows on click and disappears on valid", () => {
 
 describe("button is disabled if form is not valid", () => {
   beforeEach(() => {
-    fireEvent.change(screen.getByTestId("jobTitle"), {
+    fireEvent.change(jobTitle!, {
       target: { value: "123" },
     });
-    fireEvent.change(screen.getByTestId("jobLocation"), {
+    fireEvent.change(jobLocation!, {
       target: { value: "123" },
     });
-    fireEvent.change(screen.getByTestId("jobCompany"), {
+    fireEvent.change(jobCompany!, {
       target: { value: "123" },
     });
 
-    fireEvent.change(screen.getByTestId("jobDescription"), {
+    fireEvent.change(jobDescription!, {
       target: { value: "123" },
     });
   });
 
-  test("button is disabled if all fields are valid except job salary", async () => {
-    fireEvent.change(screen.getByTestId("jobSalary"), {
+  test("button is disabled if all fields are valid except job salary", () => {
+    fireEvent.change(jobSalary!, {
       target: { value: "123" },
     });
-    await simpleDelay(300);
-    expect(screen.getByTestId("addJobSubmitButton")).toHaveAttribute(
-      "disabled"
-    );
+    expect(submitButton).toHaveAttribute("disabled");
   });
   test("button is enabled if all fields are valid", async () => {
-    fireEvent.change(screen.getByTestId("jobSalary"), {
+    fireEvent.change(jobSalary!, {
       target: { value: "1234" },
     });
-    await simpleDelay(300);
-    expect(screen.getByTestId("addJobSubmitButton")).not.toHaveAttribute(
-      "disabled"
-    );
+    expect(submitButton).not.toHaveAttribute("disabled");
   });
 });
 
 describe("spaces do not validate the form", () => {
   test("button is disabled if every field has seven spaces", async () => {
-    fireEvent.change(screen.getByTestId("jobTitle"), {
+    fireEvent.change(jobTitle!, {
       target: { value: "ffffff" },
     });
-    fireEvent.change(screen.getByTestId("jobLocation"), {
+    fireEvent.change(jobLocation!, {
       target: { value: "ffffff" },
     });
-    fireEvent.change(screen.getByTestId("jobCompany"), {
+    fireEvent.change(jobCompany!, {
       target: { value: "ffffff" },
     });
-    fireEvent.change(screen.getByTestId("jobSalary"), {
+    fireEvent.change(jobSalary!, {
       target: { value: "ffffff" },
     });
-    fireEvent.change(screen.getByTestId("jobDescription"), {
+    fireEvent.change(jobDescription!, {
       target: { value: "ffffff" },
     });
-    await simpleDelay(300);
-    expect(screen.getByTestId("addJobSubmitButton")).not.toHaveAttribute(
-      "disabled"
-    );
+    expect(submitButton).not.toHaveAttribute("disabled");
   });
 });
 
-// describe("helper-text should appear on click", () => {
-//   test("jobTitle-helper-text should appear on click", async () => {
-//     render(<AddJob />);
-//     userEvent.click(screen.getByTestId("jobTitle"));
-//     await simpleDelay(300);
-//     expect(document.getElementById("jobTitle-helper-text")).toBeInTheDocument();
-//   });
-//   test("jobLocation-helper-text should appear on click", async () => {
-//     render(<AddJob />);
-//     userEvent.click(screen.getByTestId("jobLocation"));
-//     await simpleDelay(300);
-//     expect(
-//       document.getElementById("jobLocation-helper-text")
-//     ).toBeInTheDocument();
-//   });
-//   test("jobCompany-helper-text should appear on click", async () => {
-//     render(<AddJob />);
-//     userEvent.click(screen.getByTestId("jobCompany"));
-//     await simpleDelay(300);
-//     expect(
-//       document.getElementById("jobCompany-helper-text")
-//     ).toBeInTheDocument();
-//   });
-//   test("jobSalary-helper-text should appear on click", async () => {
-//     render(<AddJob />);
-//     userEvent.click(screen.getByTestId("jobSalary"));
-//     await simpleDelay(300);
-//     expect(
-//       document.getElementById("jobSalary-helper-text")
-//     ).toBeInTheDocument();
-//   });
-//   test("jobDescription-helper-text should appear on click", async () => {
-//     render(<AddJob />);
-//     userEvent.click(screen.getByTestId("jobDescription"));
-//     await simpleDelay(300);
-//     expect(
-//       document.getElementById("jobDescription-helper-text")
-//     ).toBeInTheDocument();
+// describe("learning testing", () => {
+//   test("onChange was called", async () => {
+//     const onChange = jest.fn();
+//     render(<input type="checkbox" onChange={onChange} />);
+//     const checkbox = screen.getByRole("checkbox");
+//     await userEvent.click(checkbox);
+//     expect(onChange).toHaveBeenCalledTimes(1);
+//     expect(checkbox).toBeChecked();
 //   });
 // });
+
+// describe("learning testing", () => {
+//   test("click was called twice", async () => {
+//     const spy = jest.fn();
+//     submitButton!.addEventListener("click", spy);
+//     await userEvent.click(submitButton!);
+//     await userEvent.click(submitButton!);
+//     expect(spy).toHaveBeenCalledTimes(2);
+//   });
+
+//   test("submit was called once", async () => {
+//     //expect(screen.getByTestId("hello")).toBeUndefined;
+//     expect(screen.queryByTestId("hello")).not.toBeInTheDocument();
+//     const spy = jest.fn();
+//     const addJobForm = screen.getByTestId("addJobForm") as HTMLInputElement;
+//     addJobForm!.addEventListener("submit", spy);
+//     await userEvent.click(submitButton!);
+//     expect(spy).toHaveBeenCalledTimes(1);
+//   });
+//});
