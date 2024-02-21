efb3f7ca5227a246907539b5c27b392319804625
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-21 21:47:13 +0100

    LoadingSpinner now accepts sxProps. LoadingSpinner in root loading.tsx is now responsive.

--- src/components/Presentation/LoadingSpinner.tsx
+++ src/components/Presentation/LoadingSpinner.tsx
@@ -1,21 +1,39 @@
-import { Box, styled } from "@mui/material";
-import ColorFactory from "../Utils/ColorFactory";
-import { orange, pink, purple } from "@mui/material/colors";
-import { FaCog, FaCloud } from "react-icons/fa";
+import { setSXValues } from "@/component-functions/set-sx-values";
+import { Box, SxProps, Theme, styled } from "@mui/material";
+import { purple } from "@mui/material/colors";
+import { FaCog } from "react-icons/fa";
 
-export default function LoadingSpinner() {
-  const FaCogStyled = styled(FaCog, {
-    name: "MuiPath", // The component name
-    slot: "root", // The slot name
-  })({});
+export default function LoadingSpinner({
+  sxSpinner,
+}: {
+  sxSpinner?: SxProps<Theme>;
+}) {
+  const sxSpinnerDefault: SxProps<Theme> = {
+    height: "300px",
+    width: "300px",
+    color: purple[100],
+    opacity: "50%",
+    animation: "spin 7s linear infinite",
+    "@keyframes spin": {
+      from: {
+        transform: "rotate(0deg)",
+      },
+      to: {
+        transform: "rotate(360deg)",
+      },
+    },
+  };
 
-  const FaCloudStyled = styled(FaCloud, {
-    name: "MuiPath", // The component name
-    slot: "root", // The slot name
-  })({});
+  const { sxSpinnerFinal } = setSXValues([
+    {
+      userValues: sxSpinner,
+      defaultValues: sxSpinnerDefault,
+      sxName: "Spinner",
+    },
+  ]);
 
   return (
-    <Box position="relative" height="400px" width="400px">
+    <Box position="relative" height="300px" width="300px">
       <Box
         sx={{
           zIndex: 2001,
@@ -24,40 +42,13 @@ export default function LoadingSpinner() {
           left: "50%",
           transform: "translate(-50%, -50%)",
         }}>
-        <FaCogStyled
-          sx={{
-            height: "400px",
-            width: "400px",
-            animation:
-              "spin 7s linear infinite,change-color 3s linear infinite",
-
-            "@keyframes spin": {
-              from: {
-                transform: "rotate(0deg)",
-              },
-              to: {
-                transform: "rotate(360deg)",
-              },
-            },
-            "@keyframes change-color": {
-              "0%": {
-                color: purple[100],
-                opacity: "50%",
-              },
-
-              "50%": {
-                color: pink[100],
-                opacity: "50%",
-              },
-
-              "100%": {
-                color: purple[100],
-                opacity: "50%",
-              },
-            },
-          }}
-        />
+        <FaCogStyled sx={sxSpinnerFinal} />
       </Box>
     </Box>
   );
 }
+
+const FaCogStyled = styled(FaCog, {
+  name: "MuiPath", // The component name
+  slot: "root", // The slot name
+})({});
