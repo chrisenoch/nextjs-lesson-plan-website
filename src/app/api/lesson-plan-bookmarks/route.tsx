9dc4eaa0e81045dbc002525356bd1472e828705b
chrisenoch <christopher.enoch@yahoo.co.uk>
2024-02-22 21:39:56 +0100

    fetchCollection# changed to firebaseGET#, firebasePOST# and firebaseDELETE#

--- src/app/api/lesson-plan-bookmarks/route.tsx
+++ src/app/api/lesson-plan-bookmarks/route.tsx
@@ -1,6 +1,10 @@
 import { getLessonPlanBookmarks } from "@/db-fake";
 import { getUserIdOnSuccessOrErrorResponse } from "@/server-only/auth/get-userId-or-error-response";
-import { fetchCollection } from "@/server-only/route-functions";
+import {
+  firebaseGET,
+  firebaseDELETE,
+  firebasePOST,
+} from "@/server-only/route-functions";
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -20,9 +24,9 @@ export async function GET(request: NextRequest) {
     return errorResponse;
   }
 
-  const fetchBookmarksPayload = await fetchCollection(
+  const fetchBookmarksPayload = await firebaseGET(
     "https://nextjs-lesson-plans-default-rtdb.europe-west1.firebasedatabase.app/lesson-plan-bookmarks.json",
-    "Successfully fetched lesson plan bookmarks",
+    "Successfully fetched lesson plan bookmarks.",
     "Unable to fetch lesson plan bookmarks.",
     "bookmarks"
   );
@@ -73,10 +77,9 @@ export async function POST(request: NextRequest) {
   }
 
   //Get bookmarks: If bookmark is present, delete bookmark. If bookmark is not present, add it.
-
-  const fetchBookmarksPayload = await fetchCollection(
+  const fetchBookmarksPayload = await firebaseGET(
     "https://nextjs-lesson-plans-default-rtdb.europe-west1.firebasedatabase.app/lesson-plan-bookmarks.json",
-    "Successfully fetched lesson plan bookmarks",
+    "Successfully fetched lesson plan bookmarks.",
     "Unable to fetch lesson plan bookmarks.",
     "bookmarks"
   );
@@ -98,92 +101,71 @@ export async function POST(request: NextRequest) {
   const userBookmarks = allBookmarks.filter(
     (bookmark) => bookmark.userId === userId
   );
-
   const existingBookmark = userBookmarks.find(
     (bookmark) => bookmark.lessonPlanId === lessonPlanId
   );
 
   //If the bookmark was not found, then it was not bookmarked in the first place. So we need to add the new bookmark.
   if (existingBookmark === undefined) {
-    try {
-      const response = await fetch(
-        "https://nextjs-lesson-plans-default-rtdb.europe-west1.firebasedatabase.app/lesson-plan-bookmarks.json",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            userId,
-            lessonPlanId,
-          }),
-        }
-      );
-
-      const data = await response.json();
-      const newBookmarks = [
-        ...userBookmarks,
-        {
-          id: data.name,
-          userId,
-          lessonPlanId,
-        },
-      ];
-
-      console.log("newbookmaks");
-      console.log(newBookmarks);
-
-      return NextResponse.json(
-        {
-          message: "Lesson plan saved",
-          isError: false,
-          bookmarks: newBookmarks,
-        },
-        { status: 200 }
-      );
-    } catch {
-      return NextResponse.json(
-        {
-          message:
-            "Failed to save lesson plan due to an error. Please contact our support team.",
-          isError: true,
-        },
-        { status: 500 }
-      );
-    }
-  }
-
-  //If get to here, we need to delete the bookmark from the json-server database.
-  try {
-    const response = await fetch(
-      `https://nextjs-lesson-plans-default-rtdb.europe-west1.firebasedatabase.app/lesson-plan-bookmarks/${existingBookmark.id}.json`,
+    const payload = (await firebasePOST(
+      "https://nextjs-lesson-plans-default-rtdb.europe-west1.firebasedatabase.app/lesson-plan-bookmarks.json",
+      "Successfully added lesson plan bookmark.",
+      "Failed to save lesson plan due to an error. Please contact our support team.",
+      "id",
       {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        userId,
+        lessonPlanId,
       }
-    );
-    const newBookmarks = userBookmarks.filter(
-      (bookmark) => bookmark.lessonPlanId !== lessonPlanId
-    );
+    )) as {
+      message: string;
+      isError: boolean;
+      id: string;
+    };
+
+    if (payload.isError) {
+      return NextResponse.json(payload, { status: 500 });
+    }
+
+    const newBookmarks = [
+      ...userBookmarks,
+      {
+        id: payload.id,
+        userId,
+        lessonPlanId,
+      },
+    ];
 
     return NextResponse.json(
       {
-        message: "Lesson plan deleted",
+        message: "Lesson plan saved",
         isError: false,
         bookmarks: newBookmarks,
       },
       { status: 200 }
     );
-  } catch (error) {
-    return NextResponse.json(
-      {
-        message:
-          "Failed to delete lesson plan due to an error. Please contact our support team.",
-        isError: true,
-      },
-      { status: 500 }
-    );
   }
+
+  //If get to here, we need to delete the bookmark
+  const payload = await firebaseDELETE(
+    `https://nextjs-lesson-plans-default-rtdb.europe-west1.firebasedatabase.app/lesson-plan-bookmarks/${existingBookmark.id}.json`,
+    "Lesson plan deleted.",
+    "Failed to delete lesson plan due to an error. Please contact our support team."
+  );
+
+  if (payload.isError) {
+    return NextResponse.json(payload, { status: 500 });
+  }
+
+  const newBookmarks = userBookmarks.filter(
+    (bookmark) => bookmark.lessonPlanId !== lessonPlanId
+  );
+
+  return NextResponse.json(
+    {
+      message: "Lesson plan deleted.",
+      isError: false,
+      bookmarks: newBookmarks,
+    },
+    { status: 200 }
+  );
 }
--- src/server-only/route-functions.tsx
+++ src/server-only/route-functions.tsx
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 import "server-only";
 
-export async function fetchCollection(
+export async function firebaseGET(
   url: string,
   successMessage: string,
   failureMessage: string,
   collectionName: string
 ) {
   try {
-    const response = await fetch(url); // Used in place of a database.
+    const response = await fetch(url);
     const data = await response.json();
     if (!response.ok) {
       return {
@@ -20,6 +20,71 @@ export async function fetchCollection(
       message: successMessage,
       isError: false,
       [collectionName]: prepareFireBase(data),
+    };
+  } catch {
+    return {
+      message: failureMessage,
+      isError: true,
+    };
+  }
+}
+
+export async function firebasePOST(
+  url: string,
+  successMessage: string,
+  failureMessage: string,
+  collectionName: string,
+  dataToPost: any
+) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToPost),
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      return {
+        message: failureMessage,
+        isError: true,
+      };
+    }
+    return {
+      message: successMessage,
+      isError: false,
+      [collectionName]: data.name,
+    };
+  } catch {
+    return {
+      message: failureMessage,
+      isError: true,
+    };
+  }
+}
+
+export async function firebaseDELETE(
+  url: string,
+  successMessage: string,
+  failureMessage: string
+) {
+  try {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      return {
+        message: failureMessage,
+        isError: true,
+      };
+    }
+    return {
+      message: successMessage,
+      isError: false,
     };
   } catch {
     return {
